f1c99fba2829b6108cd4f460e935c9074be30b25
Daniel Lee <tur95049@temple.edu>
2024-03-09 15:23:59 -0500

    add index.ejs with basic structure

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // http://docs.lyricsovh.apiary.io/
+// https://api.lyrics.ovh/v1/artist/title
+// maybe I can use image api to search for an album cover
 
 import express from "express";
 import axios from "axios";
@@ -9,6 +11,14 @@ const port = 3000;
 const API_URL = "https://api.lyrics.ovh/v1";
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+let lyricsData = "Don't wanna sober up~🎇";
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", {lyrics: lyricsData});
+});
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
